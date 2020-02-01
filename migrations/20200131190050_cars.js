de03e8d3bd6a609788cc8d190db9812637d2cb19
Jake <panamajake86@gmail.com>
2020-01-31 20:58:46 -0700

    I believe I have finished MVP and some stretch goals

--- migrations/20200131190050_cars.js
+++ migrations/20200131190050_cars.js
@@ -2,13 +2,13 @@
 exports.up = function(knex) {
   return knex.schema.createTable('cars', tbl => {
       tbl.increments('id');
-      tbl.integer('vin').unique().notNullable();
+      tbl.string('vin').notNullable();
       tbl.string('make').notNullable();
       tbl.string('model').notNullable();
       tbl.integer('mileage').notNullable();
       tbl.string('transType');
       tbl.string('titleStatus');
-  })
+  });
 };
 
 exports.down = function(knex) {
